62e4162ee2b94b4541317b919bc930884d772f7c
SMBSU <sttmeyers@gmail.com>
2023-05-14 11:40:14 -0600

    Completed 1480
    Runtime: Beats 100%
    Memory: Beats 80.95%

---
+++ leetcode/1480.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int result = 0;
+        for(int i = 0; i < nums.length; i++) {
+            result += nums[i];
+            nums[i] = result;
+        }
+        return nums;
+    }
+}
